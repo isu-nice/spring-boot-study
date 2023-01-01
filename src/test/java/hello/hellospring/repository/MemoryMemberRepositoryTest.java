e7b54da3ac46cbc41cb1e6720adc042a7f878cbd
Isu Shin <isu010614@naver.com>
2023-01-02 01:44:47 +0900

    test: findAll 테스트코드 작성

--- src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
+++ src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -5,6 +5,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 class MemoryMemberRepositoryTest {
 
     MemoryMemberRepository repository = new MemoryMemberRepository();
@@ -37,5 +39,20 @@ class MemoryMemberRepositoryTest {
 
         Member result1 = repository.findByName("springFirst").get();
         Assertions.assertThat(result1).isEqualTo(member1);
+    }
+
+    @Test
+    public void findAll() {
+        Member member1 = new Member();
+        member1.setName("springFirst");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("springSecond");
+        repository.save(member2);
+
+        List<Member> results = repository.findAll();
+
+        Assertions.assertThat(results.size()).isEqualTo(2);
     }
 }
