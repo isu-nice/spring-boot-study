f89c590f5d80fe0cac6e4ec5f73a2ae78e628fa8
Isu Shin <isu010614@naver.com>
2023-01-08 19:21:54 +0900

    feat: 서비스 계층에 트랜잭션 추가

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -2,18 +2,17 @@ package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
-// @Service
+@Transactional
 public class MemberService {
 
     private final MemberRepository memberRepository;
 
-    // @Autowired
+    // @Autowired -> 생략 가능
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
